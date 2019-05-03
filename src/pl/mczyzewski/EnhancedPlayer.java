16d665d42127346dca78e71682f4d48473efeb01
Michal Czyzewski <czy.mich@gmail.com>
2019-05-04 00:07:27 +0200

    add construktor

--- src/pl/mczyzewski/EnhancedPlayer.java
+++ src/pl/mczyzewski/EnhancedPlayer.java
@@ -3,8 +3,17 @@ package pl.mczyzewski;
 public class EnhancedPlayer
 {
     private String name;
-    private int health;
+    private int health = 100;
     private String weapon;
 
+    public EnhancedPlayer(String name, int health, String weapon)
+    {
+        this.name = name;
+        if(health <= 0 && health <= 100)
+        {
+            this.health = health;
+        }
 
+        this.weapon = weapon;
+    }
 }
